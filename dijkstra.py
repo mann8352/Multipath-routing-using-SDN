b9ea72a5fef74f7b1f223379e910959553690f08
Bharath <mann8352@gmail.com>
2016-07-01 02:55:55 +0530

    Updated at 1 July 2:56 am

---
+++ dijkstra.py
@@ -0,0 +1,170 @@
+
+#Dijkstra's shortest path algorithm
+import sys
+import heapq
+import datetime
+from graph import Graph
+from vertex import Vertex
+from loadbalancer import FlowStatistics
+from flow import FlowManagement
+
+
+class Dijkstra:
+	"""Applies Dijkstra's shortest path algorithm on the graph created from the topology"""
+	def __init__(self):
+		self.fst=FlowStatistics()
+		self.fmt=FlowManagement()
+		
+			
+	def shortest(self, v, path):
+		''' make shortest path from v.previous'''
+		#print "count: "+str(self.count)+", Time: "+str(datetime.datetime.now())
+		if v.previous:
+		    path.append(v.previous.get_id())
+		    self.shortest(v.previous, path)
+		return
+
+
+	def dijkstra(self, aGraph, start, target):
+	   	# print '''Dijkstra's shortest path'''
+	    # Set the distance for the start node to zero 
+	    start.set_distance(0)
+
+	    # Put tuple pair into the priority queue
+	    unvisited_queue = [(v.get_distance(),v) for v in aGraph]
+	    heapq.heapify(unvisited_queue)
+
+	    while len(unvisited_queue):
+	        # Pops a vertex with the smallest distance 
+	        uv = heapq.heappop(unvisited_queue)
+	        current = uv[1]
+	        current.set_visited()
+
+	        #for next in v.adjacent:
+	        for next in current.adjacent:
+	            # if visited, skip
+	            if next.visited:
+	                continue
+	            new_dist = current.get_distance() + current.get_weight(next)
+	            
+	            if new_dist < next.get_distance():
+	                next.set_distance(new_dist)
+	                next.set_previous(current)
+	               # print 'updated : current = %s next = %s new_dist = %s' \
+	                       # %(current.get_id(), next.get_id(), next.get_distance())
+	           # else:
+	                #print 'not updated : current = %s next = %s new_dist = %s' \
+	                      #  %(current.get_id(), next.get_id(), next.get_distance())
+
+	        # Rebuild heap
+	        # 1. Pop every item
+	        while len(unvisited_queue):
+	            heapq.heappop(unvisited_queue)
+	        # 2. Put all vertices not visited into the queue
+	        unvisited_queue = [(v.get_distance(),v) for v in aGraph if not v.visited]
+	        heapq.heapify(unvisited_queue)
+
+
+	def shortest_path(self, sourcehost, destnhost):
+		"""Finds the shortest path, given the source and destination node"""
+		#print "shortest_path starts: "+str(datetime.datetime.now())
+		g = Graph()
+		fs = self.fst#FlowStatistics()
+		nodelist=fs.list_of_nodes()
+		for node in nodelist:
+			g.add_vertex(node)
+
+		edge=fs.edges()
+		edgelist=fs.edges_source_dest()
+
+		#print "Toplogy started: "+str(datetime.datetime.now())
+		i=0
+		while i<len(edgelist):
+			edge_weight=fs.edge_packets(edgelist[i]['id'])
+			edge_state=fs.edge_state(edgelist[i]['id'])
+			if edge_state==1:
+				g.add_edge(edgelist[i]['source'], edgelist[i]['destn'], edge_weight)
+			i += 1
+		#print "Toplogy End: "+str(datetime.datetime.now())
+		#print "Graph start: "+str(datetime.datetime.now())
+		#print 'Graph data:'
+		for v in g:
+			for w in v.get_connections():
+				vid = v.get_id()
+				wid = w.get_id()
+				#print '( %s , %s, %3d)'  % ( vid, wid, v.get_weight(w))	
+		#print "Graph End: "+str(datetime.datetime.now())
+		self.dijkstra(g, g.get_vertex(sourcehost), g.get_vertex(destnhost))
+		target = g.get_vertex(destnhost)
+		path = [target.get_id()]
+		#print "Dijkstra End: "+str(datetime.datetime.now())
+		self.shortest(target, path)
+		#print 'The shortest path : %s' %(path[::-1])
+
+		return path
+
+
+	def anyShortestPath(self, source_host, dest_host):
+		"""returns the shortest path between a source and a destination host"""
+		path=self.shortest_path(source_host, dest_host)
+		return path
+	
+	
+	def allShortestPath(self):
+		#print "path starts: "+str(datetime.datetime.now())
+		"""returns the shortest path for each host source-destination pair"""
+		fs=self.fst#FlowStatistics()
+		fm=self.fmt#FlowManagement()
+		hostlist=fs.list_of_hosts()
+		#print "path End: "+str(datetime.datetime.now())
+		#print hostlist
+		#print hostlist[0], hostlist[len(hostlist)-1]
+		i=0
+		while i<len(hostlist):
+			j=0
+			#path=[]
+			while j<len(hostlist):
+				if hostlist[i] != hostlist[j]:
+					path=self.shortest_path(hostlist[j], hostlist[i])
+					print "path between "+hostlist[i]+" and "+hostlist[j]+" is: "
+					print path
+					print "_________________________________________________________________________"
+					#fm.addFlow(path)
+				j += 1
+			i += 1
+		
+			
+
+def main():
+	#print "starts: "+str(datetime.datetime.now())
+	start=datetime.datetime.now()
+	print "starts: "+str(datetime.datetime.now())
+		
+	Dijkstra().allShortestPath()
+	print "End: "+str(datetime.datetime.now())
+	end=datetime.datetime.now()
+	time=end-start
+	print "Execution time: "+str(time)
+	#print "ends: "+str(datetime.datetime.now())
+	'''dj=Dijkstra()
+	fs = FlowStatistics()
+	nodelist=fs.nodes()
+	hostlist=fs.list_of_hosts()
+
+	#print "node-ip         ::          node-ip"
+	i=0
+	while i<len(hostlist):
+		j=0
+		while j<len(nodelist):
+			if hostlist[i]==nodelist[j]['id']:
+				#print nodelist[j]['id']+"  ::  "+nodelist[j]['ip']
+				break
+			j += 1
+		i += 1	
+	#for i in dj.shortest_path():
+		#print i'''
+    
+if __name__ == '__main__':
+	main()
+
+  
---
+++ loadbalancer.py
@@ -0,0 +1,294 @@
+import json
+import httplib2
+import traceback
+
+class FlowStatistics:
+	"""Retrieves all data from controller using REST api"""
+	
+	def __init__(self):
+		self.stat_node_list = self.statistics()
+		response=self.get_topology()
+		self.tpnodes=response['node']
+		self.tplinks=response['link']
+
+	def statistics(self):
+		""" Get statisics of all nodes from the controller"""
+
+		node_list=[]		#list to store each switch details
+		h = httplib2.Http(".cache")
+		h.add_credentials('admin', 'admin')
+			
+		try:
+			resp, content = h.request('http://localhost:8181/restconf/operational/opendaylight-inventory:nodes/', "GET")
+			response=json.loads(content)
+			response=response['nodes']['node']
+		except:
+			traceback.print_exc()
+			return
+		
+		'no. of nodes'
+		i=0
+		while i<len(response):
+			
+			nodes={'node-id':'', 'node-connector':[]}		# dictionary to store nodes-id and corresponding interfaces' details
+			
+			nodes['node-id']=str(response[i]['id'])
+			#print nodes['node-id']
+			#print "========================================="
+			res=response[i]['node-connector']
+			
+			j=0
+			while j<len(res):
+				
+				#print res[j]
+				#print "============================================"
+				node_connector={'id':'', 'state':{'link-down':'', 'blocked':'', 'live':''}, 'port-number':'', 'status':'', 'packets-transmitted':'', 'packets-received':''}
+				
+				connector_id=str(res[j]['id']).split(':')
+				if 'LOCAL' in connector_id:
+					node_connector['id']=connector_id[0]+':'+connector_id[1]
+				else:
+					node_connector['id']=str(res[j]['id'])
+				
+				#print node_connector['id']
+				#print "==============================" 
+				
+				connector_state=res[j]['flow-node-inventory:state']
+				node_connector['state']['link-down']=str(connector_state['link-down'])
+				node_connector['state']['blocked']=str(connector_state['blocked'])
+				node_connector['state']['live']=str(connector_state['live'])
+				
+				#node_name=str(res[j]['flow-node-inventory:name'])
+				#hw_address=str(res[j]['flow-node-inventory:hardware-address'])
+				
+				node_connector['port-number']=str(res[j]['flow-node-inventory:port-number'])
+				
+				packet=res[j]['opendaylight-port-statistics:flow-capable-node-connector-statistics']['packets']
+				node_connector['packets-transmitted']=packet['transmitted']
+				node_connector['packets-received']=packet['received']
+				#print 'packets-transmitted', node_connector['packets-transmitted']
+				#print 'packets-received', node_connector['packets-received']
+					
+				if 'stp-status-aware-node-connector:status' in res[j]:
+					node_connector['status']=str(res[j]['stp-status-aware-node-connector:status'])
+				
+				nodes['node-connector'].append(node_connector)
+				j += 1
+				
+				#print '=================================================================================' 
+			
+			node_list.append(nodes)
+			i += 1
+		
+		return node_list
+
+
+	#to retrieve link-down state of a particular node-connector
+	def node_connector_state(self, nc_id):
+		for i in self.stat_node_list:
+			nc=i['node-connector']
+			for j in nc:
+				if j['id']==nc_id:
+					return j['state']['link-down']
+
+
+	#to retrieve node-connector-id corresponding to a node-id and port-number	
+	def node_connector_id(self, node_id, port_no):
+		for nc in self.stat_node_list:
+			if nc['node-id']==node_id:
+				for j in nc['node-connector']:
+					if j['port-number']==port_no:
+						return j['id']
+
+
+	#to retrieve port-number corresponding to a node-connector-id	
+	def port_number(self, nc_id):
+		for nc in self.stat_node_list:
+			for j in nc['node-connector']:
+				if j['id']==nc_id:
+						return j['port-number']
+
+
+	#to get total no. of packets transmitted and retrieved at a node-connector
+	def total_packets(self, nc_id):
+		for i in self.stat_node_list:
+			nc=i['node-connector']
+			for j in nc:
+				if j['id']==nc_id:
+					return (j['packets-transmitted'])+(j['packets-received'])
+		
+		
+	#authentication and request to get topology
+	def get_topology(self):
+			
+		h = httplib2.Http(".cache")
+		h.add_credentials('admin', 'admin')
+		try:
+			resp, content = h.request('http://localhost:8181/restconf/operational/network-topology:network-topology/', "GET")
+			response=json.loads(content)
+			response=response['network-topology']['topology'][0]
+			#print (response)
+		except:
+			traceback.print_exc()
+			return
+		
+		return response			
+
+
+	#to get list of node_id of all nodes
+	def list_of_nodes(self):
+		nodelist=[]
+		
+		j=0
+		while j<len(self.tpnodes):
+			nodelist.append(str(self.tpnodes[j]['node-id']))
+			j += 1
+		return nodelist		
+
+
+	#to get list of node_id of all hosts
+	def list_of_switches(self):	
+		switchlist=[]
+		nodelist=self.list_of_nodes()
+		for node in nodelist:
+			nlist=node.split(':') 			# host is a list of string
+			if nlist[0]!='host':
+				switchlist.append(node)
+		return switchlist
+	
+
+	#to get list of node_id of all hosts
+	def list_of_hosts(self):	
+		hostlist=[]
+		nodelist=self.list_of_nodes()
+		#print nodelist
+		for node in nodelist:
+			nlist=node.split(':') 			# host is a list of string
+			if nlist[0]=='host':
+				hostlist.append(node)
+		return hostlist
+
+
+	#to get list of edge_id of all edges 
+	def list_of_edges(self):
+		edgelist=[]
+				
+		i=0
+		while i<len(self.tplinks):
+			edgelist.append(str(self.tplinks[i]['link-id']))
+			i += 1
+		return  edgelist
+
+	
+	#to get details of each edges i.e., sorce-node and destination-node of the edge
+	def edges_source_dest(self):
+		
+		edgelist=[]
+		#print len(edges)
+		i=0
+		while i<len(self.tplinks):
+			edge_dict={'id':'', 'source':'', 'destn':''}
+			edge_dict['id']=str(self.tplinks[i]['link-id'])
+			edge_dict['source']=str(self.tplinks[i]['source']['source-node'])
+			edge_dict['destn']=str(self.tplinks[i]['destination']['dest-node'])
+			edgelist.append(edge_dict)
+			i += 1
+		#print len(edgelist)
+		return edgelist	
+		
+
+	#to get details of each edges i.e., sorce-tp and destination-tp alongwith edge-id of the edge
+	def edges(self):
+		
+		edgelist=[]
+		
+		i=0
+		while i<len(self.tplinks):
+			edge_dict={'id':'', 'source-tp':'', 'destn-tp':''}
+			edge_dict['id']=str(self.tplinks[i]['link-id'])
+			edge_dict['source-tp']=str(self.tplinks[i]['source']['source-tp'])
+			edge_dict['destn-tp']=str(self.tplinks[i]['destination']['dest-tp'])
+			edgelist.append(edge_dict)
+			i += 1
+		return edgelist	
+		
+		
+	#to get details of each nodes i.e., id, tp-id, ap-id, mac, ip
+	def nodes(self):
+		
+		nodelist=[]
+		
+		j=0
+		while j<len(self.tpnodes):
+			node_dict={'id':'', 'tp-id':[], 'ap-id':'', 'mac':'', 'ip':''} #ap=attachment point :only for hosts
+			node_dict['id']=str(self.tpnodes[j]['node-id'])
+			tp=self.tpnodes[j]['termination-point'] #tp=termination point
+			
+			#checking for hosts
+			host=self.tpnodes[j]['node-id'].split(':') 			# host is a list of string
+			if host[0]=='host':
+				node_dict['ap-id']=str(self.tpnodes[j]['host-tracker-service:attachment-points'][0]['tp-id'])
+				address=self.tpnodes[j]['host-tracker-service:addresses'][0]
+				node_dict['mac']=str(address['mac'])
+				node_dict['ip']=str(address['ip'])
+				
+			k=0
+			while k<len(tp):
+				node_dict['tp-id'].append(str(tp[k]['tp-id']))
+				k += 1
+			
+			nodelist.append(node_dict)
+			j += 1
+		
+		return nodelist
+
+
+	#to get total no. of packets transmitted and retrieved at an edge by both its node connectors
+	def edge_packets(self,edge_id):
+		edge=self.edges()
+		#print edge
+		i=packets=0
+		while i<len(edge):
+			if edge[i]['id']==edge_id:
+				try:
+					source_packets=int(self.total_packets(edge[i]['source-tp']))
+				except:
+					source_packets=0
+				try:
+					dest_packets=int(self.total_packets(edge[i]['destn-tp']))
+				except:
+					dest_packets=0
+				packets=source_packets+dest_packets
+				return packets
+			i +=1
+
+
+	#to get state of source-tp and destination-tp of an edge
+	def edge_state(self,edge_id):
+		edgelist=self.edges()
+		
+		i=0
+		while i<len(edgelist):
+			if edgelist[i]['id']==edge_id:
+				source_tp_state=self.node_connector_state(edgelist[i]['source-tp'])
+				dest_tp_state=self.node_connector_state(edgelist[i]['destn-tp'])
+				if source_tp_state=='True':
+					return 0
+				if dest_tp_state=='True':
+					return 0
+			
+				return 1
+			i += 1
+
+
+def main():
+	print "run"
+	print FlowStatistics().list_of_hosts()
+	#self.statistics()
+	#topology()
+	#dynamics()
+	FlowStatistics().edges_source_dest()
+if __name__ == '__main__':
+	main()
+
+
